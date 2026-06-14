7ea71fb77da8ef71d01ae1caf867eba56fd91f56
ahmedsami019 <easami019192@gmail.com>
2026-06-14 10:54:29 +0600

    added jwt to secure data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,9 +12,25 @@ const port = process.env.PORT || 3000
 |--------------------------------------------------------------------------
 */
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+const verifyJWTToken = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({message: "unauthorize access"})
+  }
+  const token = authorization.split(' ')[1]
+  if(!token){
+    return res.status(401).send({message: 'unauthorize access'})
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+    if(err){
+        return res.status(401).send({message: 'unauthorize access'})
+    }
+    next();
+  })
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -22,19 +38,18 @@ app.use(cors())
 |--------------------------------------------------------------------------
 */
 
-const dbUser = process.env.DB_USER
-const dbPass = process.env.DB_PASS
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
 
-const uri = `mongodb+srv://${dbUser}:${dbPass}@maincluster0.m4dyknx.mongodb.net/?appName=MainCluster0`
+const uri = `mongodb+srv://${dbUser}:${dbPass}@maincluster0.m4dyknx.mongodb.net/?appName=MainCluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-})
-
+  },
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -42,9 +57,23 @@ const client = new MongoClient(uri, {
 |--------------------------------------------------------------------------
 */
 
-app.get('/', (req, res)=>{
-    res.send('hello nature')
-})
+app.get("/", (req, res) => {
+  res.send("hello nature");
+});
+
+/*
+|--------------------------------------------------------------------------
+| jwt api
+|--------------------------------------------------------------------------
+*/
+
+app.post("/getToken", (req, res) => {
+  const loggedUser = req.body;
+  const token = jwt.sign(loggedUser, process.env.JWT_SECRET, {
+    expiresIn: "2d",
+  });
+  res.send({ token });
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -52,65 +81,65 @@ app.get('/', (req, res)=>{
 |--------------------------------------------------------------------------
 */
 
-const run = async()=>{
-    try {
+const run = async () => {
+  try {
+    // mongodb connection
+    client.connect();
 
-        // mongodb connection
-        client.connect()
+    const plantsCollection = client.db("flora_tracker_DB").collection("plants");
 
-        const plantsCollection = client.db("flora_tracker_DB").collection("plants");
-
-        /*
+    /*
         |--------------------------------------------------------------------------
         | all route
         |--------------------------------------------------------------------------
         */
 
-        app.get('/plants', async(req, res)=>{
-            const result = await plantsCollection.find().toArray()
-            res.send(result)
-        })
+    app.get("/plants", verifyJWTToken, async (req, res) => {
+      const query = {};
+      const email = req.query.email;
+      if (email) {
+        query.author_email = email;
+      }
+      const result = await plantsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        app.post('/plants', async(req, res)=>{
-            const newPlant = req.body
-            const result = await plantsCollection.insertOne(newPlant)
-            res.send(result)
-        })
+    app.post("/plants", async (req, res) => {
+      const newPlant = req.body;
+      const result = await plantsCollection.insertOne(newPlant);
+      res.send(result);
+    });
 
-        app.delete('/plants/:id', async(req, res)=>{
-            const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
-            const result = await plantsCollection.deleteOne(query)
-            res.send(result)
-        })
+    app.delete("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantsCollection.deleteOne(query);
+      res.send(result);
+    });
 
-        app.patch('/plants/:id', async(req, res)=>{
-            const id = req.params.id
-            const filter = {_id : new ObjectId(id)}
-            const updatedDoc = {
-                $set: req.body
-            }
-            const result = await plantsCollection.updateOne(filter, updatedDoc)
-            res.send(result)
-        })
+    app.patch("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: req.body,
+      };
+      const result = await plantsCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-        // send a ping to suer connection is correct
-        const ping = await client.db('flora_tracker_DB').command({ping: 1})
-        if(ping.ok === 1){
-
-            console.log('pinged you deployment. you successfully connect to the mongodb');
-        }
-        
-    } catch (error) {
-        console.error('database connection failed', error)
+    // send a ping to suer connection is correct
+    const ping = await client.db("flora_tracker_DB").command({ ping: 1 });
+    if (ping.ok === 1) {
+      console.log(
+        "pinged you deployment. you successfully connect to the mongodb",
+      );
     }
+  } catch (error) {
+    console.error("database connection failed", error);
+  }
+};
+run();
 
-}
-run()
-
-
-
-
-app.listen(port, ()=>{
-    console.log(`listening from port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`listening from port ${port}`);
+});
